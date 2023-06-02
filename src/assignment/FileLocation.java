80b7e46577fd17ec73d9475e844638364cef0cf9
Howard <howard_bb@hotmail.com>
2023-06-02 12:24:55 +0800

    feat: change the file to use relative path

--- src/assignment/FileLocation.java
+++ src/assignment/FileLocation.java
@@ -2,12 +2,12 @@ package assignment;
 
 import java.io.File;
 public interface FileLocation {
-    public static final File adminFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\Admin.txt");
-    public static final File studentFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\Student.txt");
-    public static final File userFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\User.txt");
-    public static final File reportFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\Report.txt");
-    public static final File loginRecordFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\LoginRecord.txt");
-    public static final File studentApplicationFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\StudentApplication.txt");
-    public static final File paymentFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\Payment.txt");
-    public static final File roomInfoFile = new File("C:\\Users\\dtang\\Desktop\\Year 2 Sem 1\\OODJ\\Assignment\\data\\RoomInfo.txt");
+    public static final File adminFile = new File("data/Admin.txt");
+    public static final File studentFile = new File("data/Student.txt");
+    public static final File userFile = new File("data/User.txt");
+    public static final File reportFile = new File("data/Report.txt");
+    public static final File loginRecordFile = new File("data/LoginRecord.txt");
+    public static final File studentApplicationFile = new File("data/StudentApplication.txt");
+    public static final File paymentFile = new File("data/Payment.txt");
+    public static final File roomInfoFile = new File("data/RoomInfo.txt");
 }
